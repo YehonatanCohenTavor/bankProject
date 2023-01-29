334f815a6cd9a22e7443b94a2985f1f87538dd2d
hilma <jonathanco999@gmail.com>
2023-01-29 15:53:25 +0200

    changed connection name to database

--- server/connection.js
+++ server/connection.js
@@ -1,15 +1,15 @@
 const mysql = require('mysql');
 
-const con = mysql.createConnection({
+const database = mysql.createConnection({
     host: 'localhost',
     username: 'root',
     password: 'z10mz10m',
     database:'BANK'
 })
 
-con.connect(function(err) {
+database.connect(function(err) {
     if (err) throw err;
     console.log("Connected to BANK database!");
 });
   
-module.exports = con;
+module.exports = database;
