fff28b293fc2ce391097dfb6cb661eb825364732
hilma <jonathanco999@gmail.com>
2023-02-02 07:59:19 +0200

    updating customer working

--- client/src/pages/Info.js
+++ client/src/pages/Info.js
@@ -5,7 +5,7 @@ import Loading from './components/Loading'
 import '../styles/info.css'
 import Footer from './components/Footer'
 
-function Info () {
+function Info() {
   const { authorization } = useContext(AppContext)
   const location = useLocation()
   const navigate = useNavigate()
@@ -20,7 +20,7 @@ function Info () {
   }, [])
 
   const getInfo = async () => {
-    const res = await fetch(`http://localhost:8000/customers/${user_id}`)
+    const res = await fetch(`http://localhost:8000/customers/${user_id}/customerData`)
     const data = await res.json()
     console.log(data);
     setCostumerInfo(data)
@@ -28,9 +28,9 @@ function Info () {
   }
 
   const collectChanges = async ({ target }) => {
-      const { name, value } = target;
-      setChanges(prev => ({ ...prev, [name]: value }))
-      console.log(changes);
+    const { name, value } = target;
+    setChanges(prev => ({ ...prev, [name]: value }))
+    console.log(changes);
   }
 
   const isUpdateInfo = async () => {
@@ -39,15 +39,15 @@ function Info () {
 
   const submitChanges = async e => {
     e.preventDefault()
-    const res = await fetch(`http://localhost:8000/customers/${costumerInfo.customer_id}/customerData`, {
-    method: 'PUT',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(changes)
-  })
-  if (res.ok) {
-    setUpdating(false);
-    alert('Inforamtion successfully updated');
-    navigate(`/UserPage/${user_id}`)
+    const res = await fetch(`http://localhost:8000/customers/${user_id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(changes)
+    })
+    if (res.ok) {
+      setUpdating(false);
+      alert('Inforamtion successfully updated');
+      navigate(`/UserPage/${user_id}`)
     }
     else {
       alert('Something went wrong');
@@ -69,52 +69,53 @@ function Info () {
   return (
     <div className='UserInfo'>
       <h1>Personal Information</h1>
-      {updating ? null:<button onClick={isUpdateInfo}> Change</button>}
+      {updating ? null : <button onClick={isUpdateInfo}> Change</button>}
       <form onSubmit={submitChanges}>
-      <div className='buttons-continer'>
-       {updating ? <button type='submit'>Submit</button> : null}
-       {updating ? <button onClick={cancelChanges}>Cancel Changes</button> : null}
-      </div>
-        <li className='speciel'>
-          <strong>First name</strong>
-          {costumerInfo.first_name}
-        </li>
-        <li className='speciel'>
-          <strong>Last Name</strong> {costumerInfo.last_name}
-        </li>
-        <li>
-          <strong>E-mail</strong> {updating ? (
-            <input name='email' type='email' onChange={collectChanges} />
-          ) : (
-            costumerInfo.email
-          )}
-        </li>
-        <li className='speciel'>
-          <strong>ID</strong> {costumerInfo.identity_number}
-        </li>
-        <li>
-          <strong>Address</strong> {updating ? (
-            <input name='address' type='text' onChange={collectChanges} />
-          ) : (
-            costumerInfo.address
-          )}
-        </li>
-        <li className='speciel'>
-          <strong>Birth Date</strong> 
+        <div className='buttons-continer'>
+          {updating ? <button type='submit'>Submit</button> : null}
+          {updating ? <button onClick={cancelChanges}>Cancel Changes</button> : null}
+        </div>
+        <ul className='userInfoList'>
+          <li className='special'>
+            <strong>First name</strong>
+            {costumerInfo.first_name}
+          </li>
+          <li className='special'>
+            <strong>Last Name</strong> {costumerInfo.last_name}
+          </li>
+          <li className='special'>
+            <strong>E-mail</strong> {updating ? (
+              <input name='email' type='email' onChange={collectChanges} />
+            ) : (
+              costumerInfo.email
+            )}
+          </li>
+          <li className='special'>
+            <strong>ID</strong> {costumerInfo.identity_number}
+          </li>
+          <li className='special'>
+            <strong>Address</strong> {updating ? (
+              <input name='address' type='text' onChange={collectChanges} />
+            ) : (
+              costumerInfo.address
+            )}
+          </li>
+          <li className='special'>
+            <strong>Birth Date</strong>
             {new Date(costumerInfo.birth_date).toLocaleDateString()}
-        </li>
-        <li>
-          <strong>Phone</strong> {updating ? (
-            <input name='phone' type='tel' onChange={collectChanges} pattern='^\d{3}-?\d{3}-?\d{4}$'/>
-          ) : (
-            costumerInfo.phone
-          )}
-        </li>
-        <li className='speciel'>
-          <strong>Branch</strong>
+          </li>
+          <li className='special'>
+            <strong>Phone</strong> {updating ? (
+              <input name='phone' type='tel' onChange={collectChanges} pattern='^\d{3}-?\d{3}-?\d{4}$' />
+            ) : (
+              costumerInfo.phone
+            )}
+          </li>
+          <li className='special'>
+            <strong>Branch</strong>
             {costumerInfo.branch}
-        </li>
-        
+          </li>
+        </ul>
       </form>
     </div>
   )
